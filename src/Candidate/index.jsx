ebe092ec1d97aa9e91a88f3504f6d3cddf271bc6
Lucie Musilová <musil.lucie@seznam.cz>
2021-11-05 19:29:00 +0100

    hotovo

--- src/Candidate/index.jsx
+++ src/Candidate/index.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './style.css';
 
-const Candidate = (props) => (
-  <div className="candidate">
-    <h3 className="candidate__name">{props.name}</h3>
-    <img className="candidate__avatar" src={props.avatar} />
-    <button className="btn-vote">Vybrat</button>
-  </div>
-);
+const Candidate = ({name, avatar, choosed}) => {
+  
+    const handleClick = () => {
+      choosed(name)
+    };
+  
+    return(
+      <div className="candidate">
+        <h3 className="candidate__name">{name}</h3>
+        <img className="candidate__avatar" src={avatar} />
+        <button className="btn-vote" onClick={handleClick}>Vybrat</button>
+      </div>
+    )
+  };
 
 export default Candidate;
--- src/index.jsx
+++ src/index.jsx
@@ -13,7 +13,12 @@ const App = () => {
     { name: "Beáta Skočdopolová", avatar: '/assets/candidate03.png' },
     { name: "Lubomír Poňuchálek", avatar: '/assets/candidate04.png' },
   ]), []);
+
   
+  const choosePrez = (prez) => {
+    setPresident(prez);
+  }
+
   return (
     <div className="container">
       <div className="castle">
@@ -35,6 +40,7 @@ const App = () => {
             key={c.name}
             name={c.name} 
             avatar={c.avatar} 
+            choosed={choosePrez}
           />
         ))}
       </div>
